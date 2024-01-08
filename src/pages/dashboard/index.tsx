ffe80b44be0a6b7e90ecf159887ad4c3abe6ca43
hebertsanto <hebertsantos0704@gmail.com>
2024-01-08 17:25:58 -0300

    add token verification

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,25 +1,37 @@
 import { Header } from "../../components/header";
 import { MenuBar } from "../../components/menuBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import { DashboardStyle, MainContainer } from './style';
+import { useEffect,} from "react";
 
 export const Dashboard = () => {
-    return(
+
+    const token = localStorage.getItem('token');
+    const navigate = useNavigate();
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        if (!token) {
+            navigate(`/login?error&ref=${pathname}`);
+        }
+    }, [navigate, token]);
+    
+    return (
         <DashboardStyle>
             <Header />
             <MainContainer>
-            <MenuBar />
-               <div 
-                style={{
-                    width:'80%',
-                    padding:'20px',
-                    borderRadius:'10px',
-                    boxShadow:'4px 4px 4px 4px gainsboro',
-                    backgroundColor:'#fff'
-                }}
-               >
-                  <Outlet />
-               </div>
+                <MenuBar />
+                <div
+                    style={{
+                        width: '80%',
+                        padding: '20px',
+                        borderRadius: '10px',
+                        boxShadow: '4px 4px 4px 4px gainsboro',
+                        backgroundColor: '#fff'
+                    }}
+                >
+                    <Outlet />
+                </div>
             </MainContainer>
         </DashboardStyle>
     )
